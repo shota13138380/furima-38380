f3a7b76e7e1b282fd8dccd1dd733e28284f764d6
Shota <1s2b1acksn0w@gmail.com>
2022-09-04 19:03:54 +0900

    氏名のバリエーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday,        presence: true
-
+  VALID_NAME_REGEX     = /\A([ぁ-んァ-ン一-龥]|ー)+\z/.freeze
+  VALID_KANA_REGEX     = /\A([ァ-ン]|ー)+\z/.freeze
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :password,                        format: { with: VALID_PASSWORD_REGEX }
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name,      presence: true, format: { with: VALID_NAME_REGEX }
+  validates :last_name_kana,  presence: true, format: { with: VALID_KANA_REGEX }
+  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :birthday,        presence: true
 
 end
