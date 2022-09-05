ede2bafa53d7a863599ff8666a87734d64492e72
Shota <1s2b1acksn0w@gmail.com>
2022-09-05 14:18:02 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_NAME_REGEX     = /\A([ぁ-んァ-ヶ一-龥]|ー)+\z/.freeze
+  VALID_NAME_REGEX     = /\A([ぁ-んァ-ヶ一-龥々]|ー)+\z/.freeze
   VALID_KANA_REGEX     = /\A([ァ-ン]|ー)+\z/.freeze
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password,                        format: { with: VALID_PASSWORD_REGEX }
