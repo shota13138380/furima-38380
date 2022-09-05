d5044e2da1ba0efc76634a050a87fbb333f232a4
Shota <1s2b1acksn0w@gmail.com>
2022-09-05 18:25:17 +0900

    価格のバリデーショ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,6 +16,6 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id,          numericality: { other_than: 1 , message: "can't be blank"} 
   validates :scheduled_delivery_id,  numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :price,                  numericality: { in: 300..9999999 }
+  validates :price,                  numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
 end
