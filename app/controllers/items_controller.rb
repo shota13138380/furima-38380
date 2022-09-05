ccd53bdbda7daa5d3974cd9e501ca8f5d4cc5ee9
Shota <1s2b1acksn0w@gmail.com>
2022-09-05 16:02:37 +0900

    ストロングパラメーター定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,29 @@
 class ItemsController < ApplicationController
   def index
+    
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    Item.create(item_params)
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(
+      :name, 
+      :info, 
+      :category_id, 
+      :sales_status_id, 
+      :shipping_fee_status_id, 
+      :prefecture_id, 
+      :scheduled_delivery_id, 
+      :price
+    ).merge(user_id: current_user.id)
   end
 
 end
