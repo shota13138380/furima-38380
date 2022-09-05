f065d826454c78785bdd8cba3d3c81c74c7e1c0f
Shota <1s2b1acksn0w@gmail.com>
2022-09-05 15:18:22 +0900

    itemsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :messages 
+
   VALID_NAME_REGEX     = /\A([ぁ-んァ-ヶ一-龥々]|ー)+\z/.freeze
   VALID_KANA_REGEX     = /\A([ァ-ン]|ー)+\z/.freeze
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
