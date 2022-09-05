88dc6cf3196212d9240fe4628f224bc054874edb
Shota <1s2b1acksn0w@gmail.com>
2022-09-05 17:44:28 +0900

    ビュー編集、スペルミス修正、バリデーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,11 +7,12 @@ class Item < ApplicationRecord
   belongs_to :scheduled_delivery
   
   validates :name,  presence: true
+  validates :info,  presence: true
+  validates :category_id,            numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id,        numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,          numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :scheduled_delivery_id,  numericality: { other_than: 1 , message: "can't be blank"} 
   validates :price,                  numericality: { in: 300..9999999 }
-  validates :category_id,            numericality: { other_than: 1 } 
-  validates :sales_status_id,        numericality: { other_than: 1 } 
-  validates :shipping_fee_status_id, numericality: { other_than: 1 }
-  validates :prefecture_id,          numericality: { other_than: 1 } 
-  validates :scheduled_delivery_id,  numericality: { other_than: 1 } 
 
 end
