7d2592371e9b63b6919d32add0aa03274db17d0a
Shota <1s2b1acksn0w@gmail.com>
2022-09-08 10:35:44 +0900

    購入画面のフォーム編集、不要なコントローラー削除

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,7 +2,8 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  
+    @item = Item.find(params[:item_id])
+    @purchase_destination = PurchaseDestination.new
   end
 
 end
