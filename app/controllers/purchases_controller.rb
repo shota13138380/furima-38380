b80c3738f786dfa29deea915b3ff537fb4e04cb5
Shota <1s2b1acksn0w@gmail.com>
2022-09-08 11:40:32 +0900

    クレジット決済機能追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,4 +6,31 @@ class PurchasesController < ApplicationController
     @purchase_destination = PurchaseDestination.new
   end
 
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_destination = PurchaseDestination.new(purchase_params)
+    if @purchase_destination.valid?
+      pay_item
+      @purchase_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_destination).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
