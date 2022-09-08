0696324e427074682c728ccbd3a1b859e75a13e3
Shota <1s2b1acksn0w@gmail.com>
2022-09-08 17:36:55 +0900

    テストコード修正

--- spec/models/purchase_destination_spec.rb
+++ spec/models/purchase_destination_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe PurchaseDestination, type: :model do
   before do
-    @purchase_destination = FactoryBot.build(:purchase_destination)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @purchase_destination = FactoryBot.build(:purchase_destination, user_id: user.id, item_id: item.id)
   end
 
   describe '商品の購入' do
